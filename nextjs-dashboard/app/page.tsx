e575827035d6580aed369bbb6d40fb4daf8c28a3
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2025-01-16 01:46:51 +0900

    add mobile hero img

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -33,9 +33,17 @@ export default function Page() {
           {/* Add Hero Images Here */}
           <Image 
             src="/hero-desktop.png"
-            width={1000} height={760}
+            width={1000} 
+            height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image 
+            src="/hero-mobile.png"
+            width={560} 
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
       </div>
